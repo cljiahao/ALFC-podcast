fc1840785418f0ffb316042bb2d87d2e3801a29f
cljiahao <cljiahao@hotmail.com>
2023-08-02 21:35:34 +0800

    Fixed new spotify web links

--- src/spotifyUpload.py
+++ src/spotifyUpload.py
@@ -5,9 +5,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.service import Service
 
 class spotifyUpload():
     def __init__(self,lang,srcPath,fileNames):
@@ -18,13 +17,15 @@ class spotifyUpload():
 
         spotPath = os.path.join(os.getcwd(),srcPath,lang)
 
+        service = Service()
+
         chrome_options = Options()
         chrome_options.add_argument("--window-size=1920,1080")
         chrome_options.add_argument("--start-maximized")
         chrome_options.add_argument('--remote-debugging-port=9222')
-        chrome_options.binary_location = '/usr/bin/google-chrome'
-        chrome_options.headless = True
-        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=chrome_options)
+        # chrome_options.binary_location = '/usr/bin/google-chrome'
+        chrome_options.headless = False
+        self.driver = webdriver.Chrome(service=service,options=chrome_options)
         self.wait = WebDriverWait(self.driver, 100)
         self.login(user,pwd)
         for fileName in fileNames:
@@ -35,6 +36,8 @@ class spotifyUpload():
     def login(self,user,pwd):
         
         self.driver.get("https://podcasters.spotify.com/pod/dashboard/episode/wizard")
+
+        self.driver.find_element(By.XPATH,'//*[@id="app-content"]/div/div[3]/div/button[1]').click()
 
         username = self.wait.until(EC.presence_of_element_located((By.NAME,"email")))
         password = self.driver.find_element(By.NAME,"password")
@@ -70,9 +73,13 @@ class spotifyUpload():
         explicitRadio = self.driver.find_element(By.ID,'no-explicit-content')
         self.driver.execute_script("arguments[0].checked = true;", explicitRadio)
 
-        nextButton = self.driver.find_element(By.XPATH,'//*[@id="app-content"]/div/div/footer/div/div[4]/button')
-        self.driver.execute_script("arguments[0].click();", nextButton)
-        self.driver.execute_script("arguments[0].click();", nextButton)
-        self.driver.execute_script("arguments[0].click();", nextButton)
+        nextButton1 = self.driver.find_element(By.XPATH,'//*[@id="app-content"]/div/div/footer/div/div[4]/button')
+        self.driver.execute_script("arguments[0].click();", nextButton1)
+        time.sleep(3)
+        nextButton2 = self.driver.find_element(By.XPATH,'//*[@id="app-content"]/div/div/footer/div/div[4]/button')
+        self.driver.execute_script("arguments[0].click();", nextButton2)
+        time.sleep(3)
+        publishBut = self.driver.find_element(By.XPATH,'//*[@id="app-content"]/div/div/footer/div/div[4]/button')
+        self.driver.execute_script("arguments[0].click();", publishBut)
 
         self.driver.get("https://podcasters.spotify.com/pod/dashboard/episode/wizard")
